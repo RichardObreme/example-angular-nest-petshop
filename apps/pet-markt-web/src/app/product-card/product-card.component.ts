90b76e96492afef71d2ba10e7db044d1fdfa1da5
RichardObreme <obremerichard@gmail.com>
2025-06-13 13:13:57 +0200

    feat(product-card): add product card component
    add products images
    implement searching products

---
+++ apps/pet-markt-web/src/app/product-card/product-card.component.ts
@@ -0,0 +1,13 @@
+import { Product } from '@prisma/client';
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {
+  product = input.required<Product>();
+}
