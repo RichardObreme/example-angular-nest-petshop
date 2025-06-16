f309ef438d856ee6d9ec1c07ed3bc019b5980af9
RichardObreme <obremerichard@gmail.com>
2025-06-16 14:57:26 +0200

    feat(order): create checkout component
    implement orders API in NestJS
    Create a stripe checkout session
    implement success & failure stripe payment pages
    implement order detail component

---
+++ apps/pet-markt-be/src/app/checkout/checkout.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpException,
+} from '@nestjs/common';
+import { CheckoutService } from './checkout.service';
+import { CreateCheckoutDto } from './dto/create-checkout.dto';
+import { UpdateCheckoutDto } from './dto/update-checkout.dto';
+
+@Controller('checkout')
+export class CheckoutController {
+  constructor(private readonly checkoutService: CheckoutService) {}
+
+  @Post()
+  async create(@Body() createCheckoutDto: CreateCheckoutDto) {
+    const session = await this.checkoutService.create(createCheckoutDto);
+    if (!session.url) {
+      throw new HttpException('Could not create a checkout session', 400);
+    }
+
+    return {
+      url: session.url,
+    };
+  }
+
+  @Get()
+  findAll() {
+    return this.checkoutService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.checkoutService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCheckoutDto: UpdateCheckoutDto
+  ) {
+    return this.checkoutService.update(+id, updateCheckoutDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.checkoutService.remove(+id);
+  }
+}
---
+++ apps/pet-markt-web/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -0,0 +1,34 @@
+import { afterNextRender, Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderStore } from '../../stores/order.store';
+import { ActivatedRoute } from '@angular/router';
+import { OrderDetailComponent } from '../../orders/components/order-detail/order-detail.component';
+import { CartStore } from '../../stores/cart.store';
+
+@Component({
+  selector: 'app-checkout-success',
+  imports: [CommonModule, OrderDetailComponent],
+  templateUrl: './checkout-success.component.html',
+  styleUrl: './checkout-success.component.scss',
+})
+export class CheckoutSuccessComponent implements OnInit {
+  orderStore = inject(OrderStore);
+  route = inject(ActivatedRoute);
+  cartStore = inject(CartStore);
+
+  constructor() {
+    afterNextRender(() => {
+      this.cartStore.clearCart();
+    });
+  }
+
+  ngOnInit() {
+    const orderId = this.route.snapshot.queryParamMap.get('orderId');
+
+    if (!orderId) {
+      this.orderStore.setError('No order ID found');
+      return;
+    }
+    this.orderStore.getOrder(orderId).subscribe();
+  }
+}
