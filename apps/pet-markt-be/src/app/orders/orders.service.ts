747cb7fc70b0ae24e8f318e3b3de816fc378bd4f
RichardObreme <obremerichard@gmail.com>
2025-06-17 06:17:49 +0200

    feat(API): implement backend API calls for orders
    get all
    update
    removeUnpaid

--- apps/pet-markt-be/src/app/orders/orders.service.ts
+++ apps/pet-markt-be/src/app/orders/orders.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateOrderInput } from './dto/create-order.input';
 import { UpdateOrderInput } from './dto/update-order.input';
 import { PrismaService } from '../prisma/prisma.service';
+import { DeleteOrderResp } from './dto/delete-order.resp';
+import { OrderStatus } from '@prisma/client';
 
 @Injectable()
 export class OrdersService {
@@ -36,7 +38,15 @@ export class OrdersService {
   }
 
   findAll() {
-    return `This action returns all orders`;
+    return this.prisma.order.findMany({
+      include: {
+        items: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
   }
 
   findOne(id: string) {
@@ -52,11 +62,50 @@ export class OrdersService {
     });
   }
 
-  update(id: number, updateOrderInput: UpdateOrderInput) {
-    return `This action updates a #${id} order`;
+  update(id: string, updateOrderInput: UpdateOrderInput) {
+    return this.prisma.order.update({
+      where: {
+        id,
+      },
+      data: {
+        ...updateOrderInput,
+      },
+      include: {
+        items: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async removeUnpaid(id: string): Promise<DeleteOrderResp> {
+    const order = await this.prisma.order.findUnique({
+      where: { id },
+    });
+
+    if (!order) {
+      return {
+        success: true,
+        orderId: id,
+      };
+    }
+
+    if (order.status === OrderStatus.PAYMENT_REQUIRED) {
+      await this.prisma.order.delete({
+        where: { id },
+      });
+      return {
+        success: true,
+        orderId: id,
+      };
+    }
+
+    return {
+      success: false,
+      orderId: id,
+      error: `Order is  not in ${OrderStatus.PAYMENT_REQUIRED} state`,
+    };
   }
 }
